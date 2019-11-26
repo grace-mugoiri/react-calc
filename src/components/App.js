c545e918a1865e18d3a848a766cd8674fb6ff913
grace-mugoiri <gracemugoiri@gmail.com>
2019-11-26 13:59:07 +0300

    implement handleclick and pass clickhandle to buttonpane;

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 /* eslint-disable no-unused-vars */
+
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 class App extends React.Component {
-	state = {
-		next: null,
-		total: null,
-		operation: null
-	};
+  state = {
+    next: null,
+    total: null,
+    operation: null,
+  };
 
-	handleClick = buttonName => {
-		const info = calculate(this.state, buttonName);
-		this.setState({ ...info });
-	};
+  handleClick = (buttonName) => {
+    const info = calculate(this.state, buttonName);
+    this.setState({ ...info });
+  };
 
   render() {
-		const { total, next } = this.state;
+    const { total, next } = this.state;
     return (
       <div id='app'>
         <Display res={next ? next : total } />
