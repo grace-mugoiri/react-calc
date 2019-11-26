e43a7b3c02601d3175233b16ea8324e6614a17e6
grace-mugoiri <gracemugoiri@gmail.com>
2019-11-26 13:21:59 +0300

    initialize state constructor for app

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,27 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 class App extends React.Component {
+	state = {
+		next: null,
+		total: null,
+		operation: null
+	};
+
+	handleClick = buttonName => {
+		const info = calculate(this.state, buttonName);
+		this.setState({ ...info });
+	};
+
   render() {
+		const { total, next } = this.state;
     return (
       <div id='app'>
-        <Display value={'display calculator panel'} />
-        <ButtonPanel />
+        <Display res={next ? next : total } />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
 }
 
 export default App;
+
